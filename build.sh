0e8d501f597d44a57be142e48900daeed05044d6
Albert Lu <cylu@alu-mbpr17.attlocal.net>
2018-03-26 20:15:04 -0500

    version removed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-version=$(cat rnaseq-broad/VERSION)
+version=$(cat rnaseq-broad)
 
-CONTAINER_IMAGE="sd2e/rnaseq-broad:$version"
+CONTAINER_IMAGE="sd2e/rnaseq-broad"
 
 docker build -t ${CONTAINER_IMAGE} .
